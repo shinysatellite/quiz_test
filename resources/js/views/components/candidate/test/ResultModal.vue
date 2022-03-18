7861e732f69620598e764fa993a95431ce1b0d93
shinysatellite <sws092209@outlook.com>
2022-03-18 08:00:11 +0200

    complet the testing component

---
+++ resources/js/views/components/candidate/test/ResultModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-modal id="result_modal" size="xl" body-class="p-0" hide-footer hide-header>
+    <div class="block block-rounded block-themed block-transparent mb-0">
+      <div class="block-header bg-primary-dark">
+        <h3 class="block-title">Create Quiz</h3>
+        <div class="block-options">
+          <button
+            type="button"
+            class="btn-block-option"
+            @click="$bvModal.hide('result_modal')"
+          >
+            <i class="fa fa-fw fa-times"></i>
+          </button>
+        </div>
+      </div>
+      <div class="card-box block-content" v-if="result">
+        <p>Time : {{ result.take_time }}</p>
+        <p>Score : {{ result.total_score / result.count }}</p>
+        <p>unanswered_count : {{ result.unanswered_count }}</p>
+      </div>
+      <div
+        class="block-content block-content-full text-right border-top"
+        id="my-container"
+      >
+        <b-button
+          variant="alt-primary"
+          class="mr-1"
+          @click="$bvModal.hide('result_modal')"
+          >Restart</b-button
+        >
+        <b-button variant="alt-primary" class="mr-1" to="welcome"
+          >Go to home Page</b-button
+        >
+      </div>
+    </div>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  props: ["result"]
+};
+</script>
--- resources/js/views/pages/candidate/Test.vue
+++ resources/js/views/pages/candidate/Test.vue
@@ -3,9 +3,10 @@
     <h2 class="time text-danger">Remaining Time: {{ timerCount }}</h2>
     <div>
       <div>
-        <h2>Question:{{ this.quizzes[current].text }}</h2>
+        <h2>Question:{{ quizzes[current].text }}</h2>
+        current: {{ current }} count: {{ count }}
       </div>
-      <div v-if="this.quizzes[current].type_id == 1" class="mt-5">
+      <div v-if="quizzes[current].type_id == 1" class="mt-5">
         <b-form-group label="Answers" v-slot="{ ariaDescribedby }">
           <b-form-radio
             v-model="answer"
@@ -24,28 +25,31 @@
         </b-form-group>
       </div>
       <div v-else>
+        {{ quizzes[current].answers }}
         <b-form-group label="Answers" v-slot="{ ariaDescribedby }">
           <b-form-radio
             v-model="answer"
             :aria-describedby="ariaDescribedby"
             name="some-radios"
-            value="1"
+            :value="quizzes[current].answers[0].value"
           >
-            {{}}
+            {{ quizzes[current].answers[0].text }}
           </b-form-radio>
           <b-form-radio
             v-model="answer"
             :aria-describedby="ariaDescribedby"
             name="some-radios"
-            value="2"
-            >No
+            :value="quizzes[current].answers[1].value"
+          >
+            {{ quizzes[current].answers[1].text }}
           </b-form-radio>
           <b-form-radio
             v-model="answer"
             :aria-describedby="ariaDescribedby"
             name="some-radios"
-            value="2"
-            >No
+            :value="quizzes[current].answers[2].value"
+          >
+            {{ quizzes[current].answers[2].text }}
           </b-form-radio>
         </b-form-group>
       </div>
@@ -54,25 +58,45 @@
     <div
       class="block-content block-content-full text-right border-top"
       id="my-container"
+      v-if="current < count - 1"
     >
-      <b-button variant="primary" class="mr-1" @click="next">
+      <b-button variant="primary" class="mr-1" @click="next" v-if="answer">
         Next
       </b-button>
+      <b-button variant="primary" class="mr-1" @click="skip">
+        Skip
+      </b-button>
     </div>
+    <div
+      class="block-content block-content-full text-right border-top"
+      id="my-container"
+      v-if="current == count - 1"
+    >
+      <b-button variant="primary" class="mr-1" @click="submit">
+        Submit
+      </b-button>
+    </div>
+    <ResultModal :result="result" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import ResultModal from "@/views/components/candidate/test/ResultModal";
 
 export default {
+  components: { ResultModal },
   mounted() {
     this.getInitialData();
   },
   data: () => ({
     current: 0,
     timerCount: 0,
-    answer: ""
+    answer: "",
+    answers: [],
+    unanswered_count: 0,
+    result: null,
+    finish_time: 0
   }),
   computed: {
     ...mapGetters({
@@ -80,8 +104,18 @@ export default {
     }),
     count() {
       if (this.quizzes) {
+        return this.quizzes.length;
       }
       return 0;
+    },
+    total_time() {
+      let total = 0;
+      if (this.quizzes) {
+        this.quizzes.map(item => {
+          total += item.limit_time * 1;
+        });
+      }
+      return total;
     },
     convertedTime() {}
   },
@@ -92,22 +126,58 @@ export default {
       });
     },
     startTest() {
-      let total = 0;
-      this.quizzes.map(item => {
-        total += item.limit_time * 1;
-      });
-      this.timerCount = total;
+      this.timerCount = this.total_time;
     },
     stopTest() {
-      console.log("stopTest");
+      let total_score = 0;
+      this.answers.map(item => (total_score += item));
+
+      let unanswered_count = 0;
+      if (this.current == this.count - 1) {
+        unanswered_count = this.unanswered_count;
+      } else {
+        unanswered_count =
+          this.unanswered_count * 1 + this.count * 1 - 1 - this.current;
+      }
+      this.result = {
+        count: this.count,
+        total_score: total_score,
+        unanswered_count: unanswered_count,
+        take_time: this.total_time * 1 - this.finish_time * 1
+      };
+      this.$bvModal.show("result_modal");
     },
     next() {
+      const correct = this.quizzes[this.current].correct_answer;
+      if (correct == this.answer) {
+        this.answers.push(5);
+      } else {
+        this.answers.push(0);
+      }
       this.current++;
+      this.answer = "";
+    },
+    skip() {
+      this.answers.push(0);
+      this.answer = "";
+      this.current++;
+      this.unanswered_count++;
+    },
+    submit() {
+      const correct = this.quizzes[this.current].correct_answer;
+      if (correct == this.answer) {
+        this.answers.push(5);
+      } else {
+        this.answers.push(0);
+      }
+      this.finish_time = this.timerCount;
+      this.stopTest();
     }
   },
   watch: {
     timerCount: {
       handler(value) {
+        console.log("value", value);
         if (value > 0) {
           setTimeout(() => {
             this.timerCount--;
